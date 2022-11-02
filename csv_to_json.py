fb117350e25ceecb27df6014c0e9258f950ef696
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-11-02 23:01:06 +0100

    fu

---
+++ csv_to_json.py
@@ -0,0 +1,14 @@
+from asyncore import write
+import csv
+import json
+from os import write
+
+with open("NFT Naming csv.csv" "r") as f:
+    reader = csv.reader(f)
+    
+    data = []
+    
+    for row in reader:
+        data.append({
+                        name,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          ooo
+                    })
