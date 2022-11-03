d0e123bfc9659e03685a7621b36b5efcd27b2401
flashsavitar7 <triumphedet5@gmail.com>
2022-11-02 23:05:17 -0700

    uy

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,14 +1,14 @@
 from asyncore import write
 import csv
 import json
+import pandas as pd
 from os import write
+import codecs
 
-with open("NFT Naming csv.csv" "r") as f:
-    reader = csv.reader(f)
+ifile =  open("NFT_Naming_csv.csv", "rb")
+reader = csv.reader(codecs.iterdecode(ifile, 'utf-8'))
     
-    data = []
+data = []
     
-    for row in reader:
-        data.append({
-                        name,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          ooo
-                    })
+for row in reader:
+    print(row)
