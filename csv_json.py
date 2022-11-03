5a3c8ac731b4af68b87460f34cf1f107e1f1fd5c
flashsavitar7 <triumphedet5@gmail.com>
2022-11-03 06:47:15 -0700

    hu

--- csv_json.py
+++ csv_json.py
@@ -1,5 +1,15 @@
 import csv
 import json
+
+class BlankLineSkipper(object):
+    def __init__(self, file):
+        self.file = file
+    def __iter__(self):
+        return (line for line in self.file if line.strip())
+    def read(self):
+        return ''.join(self)
+
+
 # Function to convert a CSV to JSON
 # Takes the file paths as arguments
 def make_json(csvFilePath, jsonFilePath):
@@ -7,20 +17,23 @@ def make_json(csvFilePath, jsonFilePath):
     data = []
     # Open a csv reader
     with open(csvFilePath, encoding='utf-8') as csvf:
-        csvReader = csv.reader(csvf)
+        csvReader = csv.DictReader(BlankLineSkipper(csvf))
+        header = next(csvReader)
+        print(header)
         # Convert each row into a dictionary
         # and add it to data
         for rows in csvReader:
             # Assuming a column named 'No' to
             # be the primary key
             data.append({'format': "CHIP-0007",
-                'Name':rows[1],
-                'description':rows[2],
-                'minting-tool':""
-                'series_number':rows[0],
-                'series_total':500,
-                'attributes': [{'trait-type':"gender"}, 
-                            {'value':rows[0]}]
+                'Name':rows['Filename'],
+                'description':rows['Description'],
+                #'minting-tool':rows['Team'],
+                'series_number':rows['Series Number'],
+                'series_total':420,
+                'attributes': [{'trait_type':"gender",'value':rows['Gender']}],
+                'collection':{'name':"Zuri NFT Tickets for Free Lunch", 'id':"b774f676-c1d5-422e-beed-00ef5510c64d",
+                'attributes': [{'trait_type':"description",'value':"Rewards for accomplishments during HNGi9"}]}
 
                 })
         
@@ -31,7 +44,7 @@ def make_json(csvFilePath, jsonFilePath):
 # Driver Code
 # Decide the two file paths according to your
 # computer system
-csvFilePath = r'NFT_Naming_csv.csv'
+csvFilePath = r'HNGi9_CSV_FILE.csv'
 jsonFilePath = r'Names.json'
 # Call the make_json function
 make_json(csvFilePath, jsonFilePath)
