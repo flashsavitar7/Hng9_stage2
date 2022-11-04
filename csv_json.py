b94d8ab2324e27ecda57c8f07ce085ea821e4629
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-11-04 18:45:23 +0100

    hu

--- csv_json.py
+++ csv_json.py
@@ -1,6 +1,9 @@
+
 import csv
+import hashlib
 import json
 
+#removing blank lines from csv file if any
 class BlankLineSkipper(object):
     def __init__(self, file):
         self.file = file
@@ -17,14 +20,11 @@ def make_json(csvFilePath, jsonFilePath):
     # create a dictionary
 
     data = []
-    datas = ""
-    atrr = []
-    values = []
 
     # Open a csv reader
     with open(csvFilePath, encoding='utf-8') as csvf:
         csvReader = csv.DictReader(BlankLineSkipper(csvf))
-        header = next(csvReader, 0)
+        #header = next(csvReader)
 
 
         #print(header)
@@ -32,50 +32,62 @@ def make_json(csvFilePath, jsonFilePath):
         # and add it to data
 
         for rows in csvReader:
+            Team = ""
+            team = rows ['TEAM NAMES']
+            if team.lower().startswith("team"):
+                Team = team
+                print(Team)
 
-            Team = rows['TEAM NAMES']
-            if Team.lower().startswith("team"):
-                datas = Team
-                print(datas)
 
-
-
-            attributes = rows['Attributes']
-            atrr = attributes.split(";")
-            for i in atrr:
-                value = i.split(":")
-                trait_type = value[0]
-                values = value[1]
-                print(values)
-
-            # Assuming a column named 'No' to
-            # be the primary key
+            # append the json to data
             data.append({'format': "CHIP-0007",
                 'Name':rows['Filename'],
                 'description':rows['Description'],
-                'minting-too':datas,
+                'minting-too':Team,
                 'series_number':rows['Series Number'],
                 'series_total':420,
                 'attributes': [{'trait_type':"gender",'value':rows['Gender']}, 
-                    {'trait_type':trait_type,'value':values}
+                    {'trait_type':rows['Attributes'],}
 
                 ],
                 'collection':{'name':"Zuri NFT Tickets for Free Lunch", 'id':"b774f676-c1d5-422e-beed-00ef5510c64d",
                 'attributes': [{'trait_type':"description",'value':"Rewards for accomplishments during HNGi9"}]}
 
                 })
-    
-
-
+            
     #Open a json writer, and use the json.dumps()
-    # function to dump data
-            count = 0
-            with open(jsonFilePath +str(count), 'w', encoding='utf-8') as jsonf:
-                 jsonf.write(json.dumps(data, indent=4))
+    # function to dump data        
+    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))        
+            
+    #convert json created to hash=sha256
+    with open (jsonFilePath) as i:
+        val = json.load(i)
+        for v in val:
+            dat = f"{v}"
+            json_hash = hashlib.sha256(dat.encode()).hexdigest()
+            v.update({"Hash":json_hash})
+        val.append({"Hash":json_hash})
+        
+        
+        
+    with open(jsonFilePath, "w") as i:
+        json.dump(val, i, indent=4)
+        
+    with open(jsonFilePath, "r") as openJson:
+        datar = json.load(openJson)
+    #xonvert json to csv   
+    with open('HNGi9-CSV-FILE.output.csv', "w", encoding='utf-8') as newCsv:
+        headers = datar[0].keys()
+        copy =  csv.DictWriter(newCsv, fieldnames=headers) 
+        copy.writeheader()
+        for dat in datar:
+            copy.writerow(dat) 
+                 
 # Driver Code
 # Decide the two file paths according to your
 # computer system
 csvFilePath = r'HNGi9-CSV-FILE.csv'
-jsonFilePath = r'Names.json'
+jsonFilePath = r'Names_NFT.json'
 # Call the make_json function
 make_json(csvFilePath, jsonFilePath)
