2774ce3a90af75768ce20ac80fb4ff3682fa0555
flashsavitar7 <triumphedet5@gmail.com>
2022-11-04 06:18:20 -0700

    fy

--- csv_json.py
+++ csv_json.py
@@ -15,21 +15,39 @@ class BlankLineSkipper(object):
 
 def make_json(csvFilePath, jsonFilePath):
     # create a dictionary
+
     data = []
     datas = ""
+    atrr = []
+    values = []
+
     # Open a csv reader
     with open(csvFilePath, encoding='utf-8') as csvf:
         csvReader = csv.DictReader(BlankLineSkipper(csvf))
-        header = next(csvReader)
+        header = next(csvReader, 0)
+
+
         #print(header)
         # Convert each row into a dictionary
         # and add it to data
+
         for rows in csvReader:
 
-            Team = rows['Series Number']
+            Team = rows['TEAM NAMES']
             if Team.lower().startswith("team"):
                 datas = Team
                 print(datas)
+
+
+
+            attributes = rows['Attributes']
+            atrr = attributes.split(";")
+            for i in atrr:
+                value = i.split(":")
+                trait_type = value[0]
+                values = value[1]
+                print(values)
+
             # Assuming a column named 'No' to
             # be the primary key
             data.append({'format': "CHIP-0007",
@@ -38,7 +56,10 @@ def make_json(csvFilePath, jsonFilePath):
                 'minting-too':datas,
                 'series_number':rows['Series Number'],
                 'series_total':420,
-                'attributes': [{'trait_type':"gender",'value':rows['Gender']}],
+                'attributes': [{'trait_type':"gender",'value':rows['Gender']}, 
+                    {'trait_type':trait_type,'value':values}
+
+                ],
                 'collection':{'name':"Zuri NFT Tickets for Free Lunch", 'id':"b774f676-c1d5-422e-beed-00ef5510c64d",
                 'attributes': [{'trait_type':"description",'value':"Rewards for accomplishments during HNGi9"}]}
 
@@ -48,12 +69,13 @@ def make_json(csvFilePath, jsonFilePath):
 
     #Open a json writer, and use the json.dumps()
     # function to dump data
-    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
-        jsonf.write(json.dumps(data, indent=4))
+            count = 0
+            with open(jsonFilePath +str(count), 'w', encoding='utf-8') as jsonf:
+                 jsonf.write(json.dumps(data, indent=4))
 # Driver Code
 # Decide the two file paths according to your
 # computer system
-csvFilePath = r'HNGi9_CSV_FILE.csv'
+csvFilePath = r'HNGi9-CSV-FILE.csv'
 jsonFilePath = r'Names.json'
 # Call the make_json function
 make_json(csvFilePath, jsonFilePath)
